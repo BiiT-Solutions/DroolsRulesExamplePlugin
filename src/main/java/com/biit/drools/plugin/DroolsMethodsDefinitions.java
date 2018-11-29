accefee42c20caca426a278be0e30f2f334e3ae6
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-11-29 18:06:55 +0100

    Changed dependencies to provided.

--- src/main/java/com/biit/drools/plugin/DroolsMethodsDefinitions.java
+++
@@ -1,8 +0,0 @@
-package com.biit.drools.plugin;
-
-import com.biit.plugins.interfaces.IPlugin;
-
-public interface DroolsMethodsDefinitions extends IPlugin {
-
-	public Double methodSumParameters(Double parameter1, Double parameter2);
-}
