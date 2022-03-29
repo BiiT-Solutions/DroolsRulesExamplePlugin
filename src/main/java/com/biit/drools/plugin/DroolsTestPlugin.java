2aacd948d25feb54428c14a6cb32513fe39b7646
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-03-29 17:23:09 +0200

    Using as parent project BiiT Versions

--- src/main/java/com/biit/drools/plugin/DroolsTestPlugin.java
+++ src/main/java/com/biit/drools/plugin/DroolsTestPlugin.java
@@ -1,13 +1,15 @@
 package com.biit.drools.plugin;
 
+import com.biit.plugins.BasePlugin;
+import com.biit.plugins.interfaces.IPlugin;
 import org.pf4j.Extension;
-import org.pf4j.Plugin;
-import org.pf4j.PluginWrapper;
 
 @Extension
-public class DroolsTestPlugin extends Plugin {
-	public DroolsTestPlugin(PluginWrapper wrapper) {
-		super(wrapper);
-	}
+public class DroolsTestPlugin extends BasePlugin implements IPlugin {
+	private static final String NAME = "drools-test-plugin";
 
+	@Override
+	public String getPluginName() {
+		return NAME;
+	}
 }
